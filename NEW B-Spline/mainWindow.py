131d6317a0de6558361b1e5f3ca56c8f8da1ac8b
Rodrigo monroy <ruimonroy@csu.fullerton.edu>
2017-03-30 16:25:57 -0700

    newest implementation of Bspline

---
+++ NEW B-Spline/mainWindow.py
@@ -0,0 +1,49 @@
+
+from MyPackages.qtPackage.windowLayoutSpline import Ui_MainWindow
+from PyQt5.QtCore import Qt , QTimer, pyqtSlot
+from DrawingWindow import *
+import sys
+
+class MainWindow(Ui_MainWindow):
+    """docstring for myWindow."""
+    def __init__(self,window):
+        Ui_MainWindow.__init__(self)
+        self.setupUi(window)
+        window.setWindowTitle("BSpline")
+        self.undoButton.clicked.connect(self.pop)
+        self.editCheckBox.stateChanged.connect(self.editState)
+        self.ClearButton.clicked.connect(self.clear)
+        self.computeButton.clicked.connect(self.computeSpline)
+        self.comboBox.addItem("periodic uniform")
+        self.comboBox.addItem("open uniform")
+        self.comboBox.addItem("open uniform bezier")
+        self.popClicks = 0
+
+    def pop(self):
+        stack = self.openGLWidget.userDefinedPoints
+        if len(stack) > 0:
+            stack.pop()
+            if len(self.openGLWidget.curvePoints) > 0 :
+                self.openGLWidget.curvePoints.pop()
+            if len(self.openGLWidget.history) > 0 :
+                self.openGLWidget.curvePoints =[]
+                self.openGLWidget.history.pop()
+        self.openGLWidget.updateGL()
+
+    def clear(self):
+        self.openGLWidget.history=[]
+        self.openGLWidget.userDefinedPoints =[]
+        self.openGLWidget.curvePoints =[]
+        self.openGLWidget.updateGL()
+
+
+    def editState(self,state):
+        if state == Qt.Checked:
+            self.openGLWidget.setCursor(QtGui.QCursor(QtCore.Qt.OpenHandCursor))
+            self.openGLWidget.setEditFlag(True)
+        else:
+            self.openGLWidget.setEditFlag(False)
+        self.openGLWidget.updateGL()
+
+    def computeSpline(self):
+        self.openGLWidget.computeSpline(degree = self.spinBox.value(), knotVectorType = self.comboBox.currentText())
